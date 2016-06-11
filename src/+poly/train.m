07fa7e149631d15352052a9127de0abe210f69b5
gaffey <liws13@mails.tsinghua.edu.cn>
2016-06-11 16:10:50 +0800

    modify the structure

--- src/+poly/train.m
+++ src/+poly/train.m
@@ -1,10 +1,10 @@
 function model = train(data, degrees)
-    p = 2;
-    A = [data data.^2 ones(size(data, 1) 1)];
+    p = 5;
+    A = polybase(data, p);
     weight = (A'*A)\(A'*y);
-    regression = [data data.^2, ones(size(data, 1), 1)] * weight;
+    regression = A * weight;
     sigma_n = sum((regression - degrees).^2)/size(data, 1);
-    sigma_p = diag(2*ones(1, size(data, 2)*2+1), 0).^2;
+    sigma_p = diag(2*ones(1, size(data, 2)*p+1), 0).^2;
     TrainSet_new = polybase(data, p);
     save polydata.mat TrainSet_new data sigma_p p sigma_n
 
