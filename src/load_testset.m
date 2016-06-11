10391c812b191e28c96e85810c6b3a232be9febc
Thomas Lee <lisihan969@gmail.com>
2016-06-11 19:51:01 +0800

    Fix loads

--- src/load_testset.m
+++ src/load_testset.m
@@ -1,5 +1,14 @@
-function data = load_testset(img_list, label_dir)
-    data = [];
-    for filename = importdata(img_list)'
-        data = [data; load_data(filename, label_dir)];
+function [P, F] = load_testset(img_list, label_dir)
+    filenames = importdata(img_list);
+    count = size(filenames, 1);
+
+    P = zeros(count, 98);
+    F = zeros(count, 5);
+
+    for k = 1:count
+        filename = filenames{k};
+        [points, features] = load_data(filename, label_dir);
+
+        P(k, :) = points(:);
+        F(k, :) = features;
     end
--- src/load_trainset.m
+++ src/load_trainset.m
@@ -1,12 +1,17 @@
-function [data, degrees] = load_trainset(img_list, label_dir)
+function [P, F, degrees] = load_trainset(img_list, label_dir)
     filenames = importdata(img_list);
     count = size(filenames, 1);
-    data = zeros(count, 5);
+
+    P = zeros(count, 98);
+    F = zeros(count, 5);
     degrees = zeros(count, 1)
 
     for k = 1:count
         filename = filenames{k};
-        data(k, :) = [data; load_data(filename, label_dir)];
-        tokens = regexp(file.name, 'PM([+-]\d{2})_EN', 'tokens');
-        degrees(k) = [degrees; str2num(tokens{1}{1})];
+        tokens = regexp(filename, 'PM([+-]\d{2})_EN', 'tokens');
+        [points, features] = load_data(filename, label_dir);
+
+        P(k, :) = points(:);
+        F(k, :) = features;
+        degrees(k) = str2num(tokens{1}{1});
     end
