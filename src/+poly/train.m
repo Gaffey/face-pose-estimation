b242d54c2413f85385cd8e4c083e5b23bdd14b38
Thomas Lee <lisihan969@gmail.com>
2016-06-12 00:45:28 +0800

    Small bug fix.

--- src/+poly/train.m
+++ src/+poly/train.m
@@ -6,7 +6,6 @@ function model = train(data, degrees)
     sigma_n = sum((regression - degrees).^2)/size(data, 1);
     sigma_p = diag(2*ones(1, size(data, 2)*p+1), 0).^2;
     TrainSet_new = poly.polybase(data, p);
-    save polydata.mat TrainSet_new data sigma_p p sigma_n
 
     model = struct('p', p, ...
                    'sigma_n', sigma_n, ...
