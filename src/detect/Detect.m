16ca085d428f4dd77c3f4ec4a5d9706cb61cd9d0
Thomas Lee <lisihan969@gmail.com>
2016-06-12 23:34:16 +0800

    Recover Detect.m

---
+++ src/detect/Detect.m
@@ -0,0 +1,16 @@
+function Detect(obj, txt)
+    load net
+    frame = getsnapshot(obj);
+    stoppreview(obj);
+    frame = imresize(frame, 0.25);
+    imwrite(frame,'snapshot.jpg','jpg');% ±£¥Ê÷°
+    system('IntraFaceDetector.exe');
+    points = load('snapshot.txt');
+    hight = abs(mean(points([47:49], 2)) - mean(points([3 8], 2)));
+    points = points / hight;
+    points = bsxfun(@minus, points, mean(points));
+    xs = points(:);
+    angle = sim(net, xs);
+    set(txt,'Visible','on');
+    set(txt,'String',sprintf('%.3f',angle));
+end
